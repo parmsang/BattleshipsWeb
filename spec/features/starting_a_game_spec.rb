300b1dd36cb3c87e1421e2a4bed505d4a05019cf
Balint Nemes <squarebe03@gmail.com>
2015-08-04 14:16:35 +0100

    added feature tests

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -6,4 +6,20 @@ feature 'Starting a new game' do
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
   end
+
+  scenario 'registers a name' do
+    visit '/'
+    click_link 'New Game'
+   	fill_in :name, with: 'Dan'
+   	click_on 'Submit'
+   	expect(page).to have_content "Hello, Dan!"
+  end
+
+  scenario 'no name entered' do
+  	visit '/'
+    click_link 'New Game'
+   	fill_in :name, with: ''
+   	click_on 'Submit'
+   	expect(page).to have_content "Hello, !"
+  end
 end
